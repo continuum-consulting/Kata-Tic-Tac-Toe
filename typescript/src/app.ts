24fde7e52c38e7dd6d22e63b6201ddf563329b00
wouterroothooft <wouter.roothooft@jidoka.be>
2023-10-01 17:05:16 +0200

    Typescript: Node.js readline

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,1 +1,11 @@
 console.log("Hello world");
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+readline.question('What is your name? ', (name: string) => {
+    console.log(`Hi ${name}`);
+    readline.close();
+})
