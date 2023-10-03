53ff948d56b42905162078326a050d1983082ac9
wouterroothooft <wouter.roothooft@jidoka.be>
2023-10-03 16:54:49 +0200

    Typescript: Package added to read prompt

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,11 +1,17 @@
+import promptSync from "prompt-sync";
+
 console.log("Hello world");
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// })
+//
+// readline.question('What is your name? ', (name: string) => {
+//     console.log(`Hi ${name}`);
+//     readline.close();
+// })
 
-readline.question('What is your name? ', (name: string) => {
-    console.log(`Hi ${name}`);
-    readline.close();
-})
+const prompt = promptSync({ sigint: true });
+const name = prompt("What is your name? ");
+console.log(`Hi ${name}`);
